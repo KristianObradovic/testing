6546459cfe80efac37696cc9f70cfedee51c1c45
Sada <sadinho88@hotmail.com>
2019-01-03 19:05:48 +0100

    Update Sada

--- blackjacktest/src/blackjacktest/Meny.java
+++ blackjacktest/src/blackjacktest/Meny.java
@@ -8,9 +8,10 @@ public class Meny {
 		String create_username;
 		String create_password;
 		String tage;
+		int chance=0;
+		boolean approved= false;
 		
-		
-		public void age() 
+		public boolean age() 
 		{
 		System.out.println("\nEnter your age : ");		
 		
@@ -21,8 +22,9 @@ public class Meny {
 		
 				if(nage>=18) 
 				{
-					System.out.println("Successful registration");
-					to_login();
+					
+					approved=true;
+					
 				}
 				else if(nage<18)				
 						{
@@ -41,6 +43,7 @@ public class Meny {
 				
 				
 			}	
+			return approved;
 		}
 
 	
@@ -48,22 +51,22 @@ public class Meny {
 	public void  create_login() {
 		
 		
-			
-		//String create_username;
-		//String create_password;
+
 		
 			
 			System.out.println("\nPlease register a Username");
 			create_username=input.next();
 			System.out.println("\nPlease register a Password");
 			create_password=input.next();
-	
+			System.out.println("Successful registration");
+			
 	}
 	
-	public void to_login() {
+	public boolean to_login() {
 			
-			//to_login
-		
+			
+			while(chance<3) {
+			
 			String login_username;
 			System.out.println("\nEnter your Username");
 			login_username=input.next();
@@ -71,18 +74,49 @@ public class Meny {
 			System.out.println("\nEnter your Password");
 			login_password=input.next();
 			
-			if(create_username.equals(login_username) && create_password.equals(login_password)) 
-			{
+			
+				
+			if(create_username.equals(login_username) && create_password.equals(login_password)) { 
+			
 				System.out.println("    Login Successful     loading 100 %");	
-			}
+				chance=4;
+				approved = true;
+				
+				
+			}//if
 			
-			else 
+			else  
 			{
-				System.out.println("invalid Username or Password");
-				to_login();
-			}
+				// * * * Threee Chances * * *
+				
+				System.out.println("\nInvalid Username OR Password");
+				
+				
+					chance++;
+				 
+				if (chance==3) {
+					System.out.println("\n\nAccess denied! ");
+					System.exit(0);
+				}
+				
+				else if(chance==2){//else if
+					System.out.println("\nYOU ONLY HAVE ONE MORE CHANCE");
+					
+				}//else if
+				
+			}//else
+				
 			
-	}
+					
+	
+		}//While loop close brackets
+				
+				return approved;
+				
+
+
+	}//to_login()
+	
 		
 	
 		public void rules() {
@@ -90,10 +124,15 @@ public class Meny {
 			// -----  Header -----------
 			System.out.println("\n\n           * * * Rules * * * \n\n");
 			System.out.println("Black Jack is played with one deck consisting of 52 cards."
-					+ "\nACES can be counted as 1 or 11 points tens and face cards count as ten points."
-					+ "\nCards from 2-9 have the same value as shown on the card."
-					+ " \nYour goal is to get Black Jack."
-					+ "\nBlack Jack is the highest hand and it consists of an ACE and any 10 point card.");
+					+ "\nACES can be counted as 1 or 11 points. Tens and face cards count as ten points."
+					+ "\nCards from 2-9 have the value as the number shown on each of the cards."
+					+ "\nSo if User gets a card showing 2 then its 2 points" 
+					+ " \nYour goal is to beat the Dealers hand."
+					+ "\nIt means that your end goal is to have the highest score"
+					+ "\n21 points its the best hand you can get"
+					+ "\nTo be able to get 21 points you need to have a hand that consists of an ACE and any 10 point card."
+					+ "\nYou can always ask the dealer for more cards as long as you under 21 points."
+					+ "\nIf you get more then 21 points you will loose");
 		}
 	
 	
@@ -104,12 +143,25 @@ public class Meny {
 		
 			System.out.println("Welcome to    B l a c k - J a c k    Casino ( 18 + ) ");
 		
-			Meny meny= new Meny();
-			meny.create_login();
-			System.out.println("Successful registration");
+			Meny meny = new Meny();
+			
 			meny.age();
-			meny.rules();
-
+			
+			if(meny.approved=true){
+			
+				meny.create_login();
+			}
+			
+			
+			
+			meny.to_login();
+			if(meny.approved=true){
+				
+				meny.rules();
+				}
+			
+			
+			
 			
 		}
 		
