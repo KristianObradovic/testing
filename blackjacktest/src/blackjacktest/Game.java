26342e9b85f3866748ee542a439ba7c4ae24c26b
Sada <sadinho88@hotmail.com>
2019-01-04 13:43:04 +0100

    Class -- Game + main + Meny /Sada

--- blackjacktest/src/blackjacktest/Game.java
+++ blackjacktest/src/blackjacktest/Game.java
@@ -1,40 +1,35 @@
 package blackjacktest;
 
-import java.util.Scanner;
+
 
 public class Game {
 
 	public static void main(String[] args) {
 		
-		Meny meny= new Meny();
-		CardDeck cd = new CardDeck();
-		Scanner inp = new Scanner(System.in);
-		
-		boolean menu = true;
 		System.out.println("Welcome to    B l a c k - J a c k    Casino ( 18 + ) ");
-		while(menu);
+	
+		Meny meny = new Meny();
+		
+		
 		meny.age();
-		if(meny.approved=true)
 		
+		if(meny.approved=true){
 		
-		System.out.println("What would you like to do?/n[1] Log in/n[2] Create user");
-		String logchoice = inp.next();
-		
-		try {
-			int ichoice = Integer.parseInt(logchoice);
-			if (ichoice == 1)
-				meny.to_login();
-				
-			else if (ichoice == 2)
-				meny.create_login();
-				
-			else
-			System.out.println("Invalid number. Plase try again");
-			}
-		catch (Exception e) {
-			System.out.println("Choice out of range. Plase try again");
+			meny.create_login();
 		}
 		
-	}
-
+		
+		
+		meny.to_login();
+		if( meny.approved=true ) {
+			
+			meny.rules();
+			
+		}
+		
+		
+		
+		
+	} // main close bracket
+	
 }
