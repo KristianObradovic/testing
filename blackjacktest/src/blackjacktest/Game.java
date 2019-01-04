6bc73b65757abbaec98df7d6fddb5b9315aefa78
Vamsi <Vamsi@LAPTOP-0S092R66>
2019-01-04 17:32:46 +0100

    Formated.

--- blackjacktest/src/blackjacktest/Game.java
+++ blackjacktest/src/blackjacktest/Game.java
@@ -1,36 +1,26 @@
 package blackjacktest;
 
-
-
 public class Game {
 
 	public static void main(String[] args) {
-		
+
 		System.out.println("Welcome to    B l a c k - J a c k    Casino ( 18 + ) ");
-	
+
 		Meny meny = new Meny();
-		
-		
-		
+
 		meny.create_login();
-		if(meny.approved=true){
-		
-		
+		if (meny.approved = true) {
+
 			meny.age();
 		}
-		
-		
-		
+
 		meny.to_login();
-		if( meny.approved=true ) {
-			
+		if (meny.approved = true) {
+
 			meny.rules();
-			
+
 		}
-		
-		
-		
-		
+
 	} // main close bracket
-	
+
 }
